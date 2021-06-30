001f0861b46bfc8b1f16aa1d6f24c22797731764
joaozangeli <joaoegidio46@gmail.com>
2021-06-30 20:47:00 -0300

    30/06/2021

--- src/main/java/com/senac/projetopadrao/ProjetopadraoApplication.java
+++ src/main/java/com/senac/projetopadrao/ProjetopadraoApplication.java
@@ -9,8 +9,33 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 public class ProjetopadraoApplication {
 
 	public static void main(String[] args) {
+		int numero1 = 10;
+		int numero2 = 20;
 
-	    SpringApplication.run(ProjetopadraoApplication.class, args);
+		somaNumeros(numero1, numero2);
+		divisaoNumeros(numero1, numero2);
+		subtracaoNumeros(numero1, numero2);
+		System.out.println(subtracaoNumeros(numero1, numero2));
+		System.out.println(divisaoNumeros(numero1, numero2));
+		System.out.println(somaNumeros(numero1, numero2));
 	}
+
+	private static int subtracaoNumeros(int numero1, int numero2) {
+		int subtracao = numero2 - numero1;
+		return subtracao;
+	}
+
+	private static int divisaoNumeros(int numero1, int numero2) {
+		int divisao = numero2/numero1;
+
+		return divisao;
+	}
+
+	private static int somaNumeros(int numero1, int numero2) {
+		int soma = numero1 + numero2;
+
+		return soma;
+	}
+
 
 }
---
+++ src/main/java/com/senac/projetopadrao/model/Carro.java
@@ -0,0 +1,24 @@
+package com.senac.projetopadrao.model;
+
+public class Carro {
+    private String cor;
+    private int ano;
+    private boolean andando;
+
+
+
+    public void cor() {
+        this.cor = "magenta";
+    }
+    public void ano() {
+        this.ano = 2000;
+
+    }
+
+    public void andando() {
+        this.andando = true;
+    }
+
+
+
+}
